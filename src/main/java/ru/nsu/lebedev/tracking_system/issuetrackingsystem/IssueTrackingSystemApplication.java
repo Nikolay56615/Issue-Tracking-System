c5c1eb0f78d8c7407fff62b95a62c8b464a443d7
Nikolay56615 <75627798+Nikolay56615@users.noreply.github.com>
2025-10-29 13:27:22 +0700

    fix problem with expired data base

---
+++ src/main/java/ru/nsu/lebedev/tracking_system/issuetrackingsystem/IssueTrackingSystemApplication.java
@@ -0,0 +1,14 @@
+package ru.nsu.lebedev.tracking_system.issuetrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class IssueTrackingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueTrackingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nsu/lebedev/tracking_system/issuetrackingsystem/IssueTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.lebedev.tracking_system.issuetrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

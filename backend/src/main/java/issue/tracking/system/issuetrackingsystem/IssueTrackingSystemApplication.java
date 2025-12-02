f229776f6a6f1a699152a3583c381f80741c13ab
Nikolay56615 <75627798+Nikolay56615@users.noreply.github.com>
2025-12-02 21:46:57 +0700

    adding .env

--- backend/src/main/java/issue/tracking/system/issuetrackingsystem/IssueTrackingSystemApplication.java
+++ backend/src/main/java/issue/tracking/system/issuetrackingsystem/IssueTrackingSystemApplication.java
@@ -3,9 +3,12 @@ package issue.tracking.system.issuetrackingsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class IssueTrackingSystemApplication {
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})class IssueTrackingSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IssueTrackingSystemApplication.class, args);

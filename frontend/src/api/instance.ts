5fbbd3e802276520f859ada0a186c771394ca83d
Ivan <Streafyhs@gmail.com>
2025-12-17 09:26:13 +0700

    Add auth token creation and interceptor for requests

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -4,3 +4,11 @@ export const axiosInstance = axios.create({
   baseURL: 'http://localhost:8080/api',
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    config.headers.Authorization = `Basic ${token}`;
+  }
+  return config;
+});

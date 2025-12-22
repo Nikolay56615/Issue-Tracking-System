e11bbb87092b9d9af1fcf91c8ca3c65bfd2fe81f
streafy <streafyhs@gmail.com>
2025-12-23 00:15:12 +0700

    issue dialog wip

--- frontend/src/features/board/ui/issue-dialog.tsx
+++ frontend/src/features/board/ui/issue-dialog.tsx
@@ -7,6 +7,8 @@ import {
 } from '@/components/ui/dialog.tsx';
 import type { Issue } from '@/features/board/model';
 import { capitalize } from '@/lib/utils.ts';
+import { TypeBadge } from '@/features/board/ui/type-badge.tsx';
+import { PriorityBadge } from '@/features/board/ui/priority-badge.tsx';
 
 interface IssueDialogProps {
   issue: Issue;
@@ -18,17 +20,21 @@ export const IssueDialog = ({ issue }: IssueDialogProps) => {
   return (
     <Dialog>
       <DialogTrigger>
-        <span className="font-medium">{name}</span>
+        <span className="cursor-pointer font-medium hover:underline">
+          {name}
+        </span>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>{name}</DialogTitle>
         </DialogHeader>
-        <div>
+        <div className="flex flex-col">
           <span>Id: {id}</span>
           <span>Project: {projectId}</span>
-          <span>Type: {capitalize(type)}</span>
-          <span>Priority: {capitalize(priority)}</span>
+          <div className="flex gap-2">
+            <TypeBadge type={type} />
+            <PriorityBadge priority={priority} />
+          </div>
           <span>Status: {capitalize(status)}</span>
           <span>Description</span>
           <p>{description}</p>
